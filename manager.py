5b92eced9a8822592d75ee7888f564dfef748f63
Justin McGough <72327349+J-Maker1@users.noreply.github.com>
2022-03-05 11:43:21 -0700

    Added base of peer-to-peer connection

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 #importing resources for the manager
 from socket import *
+from sre_constants import SUCCESS
 
 #setting port for manager
 serverPort = 14000
@@ -37,29 +38,35 @@ while True:
                 reply = "ERROR: Port already in use on IP!"
                 serverSocket.sendto(reply.encode(), clientAddress)
             else:
-                newPlayer = (str(command[1]), str(command[2]), str(command[3]))
+                newPlayer = (str(command[1]), str(command[2]), str(command[3]), "Not In Game")
                 players.append(newPlayer)
                 reply = "SUCCESS,You have been registered as " + str(command[1]) + " with IPv4 " + str(command[2]) + " on port " + str(command[3])
                 serverSocket.sendto(reply.encode(), clientAddress)
+
+    
     #Query players, just counts the number of players and builds a string to best print out the data of the players and sends the string back to the client
     elif command[0] == "query players":
         playerList = ""
         for x in players:
-            playerList += "User: " + x[0] + ", " + "IPv4: " + x[1] + ", " + "Port: " + x[2] + "\n"
+            playerList += "User: " + x[0] + ", " + "IPv4: " + x[1] + ", " + "Port: " + x[2] + ", " + x[3] + "\n"
         reply = "Players: " + str(len(players)) + "\n" + playerList
         serverSocket.sendto(reply.encode(), clientAddress)
+
+
     #Query games, just counts the number of games and builds a string to best print out the data of the players and sends the string back to the client
     elif command[0] == "query games":
         gameList = ""
         for x in games:
-            gameList += "Game ID: " + x + "Dealer: " + x[0] + ", " + "Player 1: " + x[1] + ", " + "Player 2: " + x[2] + "Player 3: " + x[3] + "\n"
+            gameList += "Game ID: " + x[0] + "Dealer: " + x[1] + ", " + "Player 1: " + x[2] + ", " + "Player 2: " + x[3] + "Player 3: " + x[4] + "\n"
         reply = "Games: " + str(len(games)) + "\n" + gameList
         serverSocket.sendto(reply.encode(), clientAddress)
+
+
     #De-register checks for if the player is in a game, if not then it checks for if the player actually exists, if they do, then the server removes them from the player list. If there's an issue, it creates an error messege
     elif command[0] == "de-register":
         userPlayingGame = False
         for x in games:
-            if x[0] == command[1] or x[1] == command[1] or x[2] == command[1] or x[3] == command[1]:
+            if x[1] == command[1] or x[2] == command[1] or x[3] == command[1] or x[4] == command[1]:
                 userPlayingGame = True
         
         if userPlayingGame == False:
@@ -76,8 +83,52 @@ while True:
         else:
             reply = "Error: User currently in game!"
         serverSocket.sendto(reply.encode(), clientAddress)
-                    
+
+
+    if command[0] == "start":
+        searchNumber = int(command[1])
+        playersFound = []
+        for index, player in enumerate(players):
+            if searchNumber == 0:
+                break
+            if player[3] == "Waiting For Game":
+                playersFound.append(index)
+                searchNumber = searchNumber - 1
+       
+        if searchNumber != 0:
+            reply = "Error: Not enough available players."
+        else:
+            reply = "SUCCESS"
+            for index, player in enumerate(players):
+                if player[0] == command[2]:
+                    reply += "," + player[1] + "-" + player[2]
+                    y = list(player)
+                    y[3] = "In Game"
+                    x = tuple(y)
+                    players[index] = x
+            for x in playersFound:
+                y = list(players[x])
+                y[3] = "In Game"
+                players[x] = tuple(y)
+                reply += "," + players[x][1] + "-" + players[x][2]
+        serverSocket.sendto(reply.encode(), clientAddress)
+
+    if command[0] == "waiting":
+        reply = "Error"
+        for index, player in enumerate(players):
+            if player[0] == command[1] and player[3] == "Not In Game":
+                y = list(player)
+                y[3] = "Waiting For Game"
+                x = tuple(y)
+                players[index] = x
+                reply = "SUCCESS"
+            if player[0] == command[1] and player[3] == "Waiting For Game":
+                y = list(player)
+                y[3] = "Not In Game"
+                x = tuple(y)
+                players[index] = x
+                reply = "SUCCESS"
+        serverSocket.sendto(reply.encode(), clientAddress)
             
-        
-        
-    
+            
+
--- peer.py
+++ peer.py
@@ -1,6 +1,7 @@
 #importing resources for the peer software to use
 from socket import *
 import sys
+import time
 
 #recording the arguments for reaching the manager
 managerName = sys.argv[1]
@@ -8,13 +9,16 @@ managerPort = int(sys.argv[2])
 
 registered = False
 playerSocket = socket(AF_INET, SOCK_DGRAM)
+playerName = ""
 
+def play_game():
+    print("Test")
 
 print(
-"1: Register \n2: Query Players \n3: Query Games \n4: De-register \n5: Exit"
+"0: Exit \n1: Register \n2: Query Players \n3: Query Games \n4: De-register \n5: Start A Game \n6: Join A Game"
 )
 userIn = input('Enter Command: ')
-while userIn != 5:
+while userIn != 0:
 
     #Condition for registering. A username, IP, and Port are requested, a command is sent, and a reply is returned. Based on the reply, the conditional statement will notify the user of it and adjust some variables for future use of the command
     if userIn == '1':
@@ -38,6 +42,7 @@ while userIn != 5:
             #if success we notify the user and make an official player socket bound to an open port specified from the server
             if returnmessege[0] == "SUCCESS":
                 playerSocket.bind(('', int(inputPort)))
+                playerName = username
                 print(returnmessege[1])
             #if reply is a failure
             else:
@@ -86,8 +91,68 @@ while userIn != 5:
         print(returnmessege)
 
         userIn = input('Enter Command:')
+
+    #condition to query the start of a game
     elif userIn == '5':
+        if registered == True:
+            players = input('How many players do you wish to play with? 1-3: ')
+            if players != '1' and players != '2' and players != '3':
+                print("Error: Invalid number of players requested.\n")
+            else:
+                messege = "start," + players + "," + playerName
+                clientSocket = socket(AF_INET, SOCK_DGRAM)
+                clientSocket.sendto(messege.encode(),(managerName, managerPort))
+                modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+                clientSocket.close()
+                returnmessege = modifiedMessage.decode().split(',')
+                sendmessege = modifiedMessage.decode()
+                if returnmessege[0] == "SUCCESS":
+                    print("Game created")
+                    for x in range(int(players)):
+                        friend = returnmessege[2+x].split('-')
+                        clientSocket = socket(AF_INET, SOCK_DGRAM)
+                        clientSocket.sendto(sendmessege.encode(),(friend[0], int(friend[1])))
+                        clientSocket.close()
+                        print("Sent to: " + friend[0] + " At port: " + friend[1])
+                else:
+                    print(returnmessege)
+        else:
+            print("Error: Must be registered to start a game.")
+        userIn = input('Enter Command:')
+
+    #An extra adition to switch the user into listening mode to be pulled into a game
+    elif userIn == '6':
+        if registered == True:
+            messege = "waiting," + playerName
+            clientSocket = socket(AF_INET, SOCK_DGRAM)
+            clientSocket.sendto(messege.encode(),(managerName, managerPort))
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+            clientSocket.close()
+            returnmessege = modifiedMessage.decode()
+            if returnmessege == "SUCCESS":
+                print("Waiting for game...")
+                playerSocket.settimeout(10.0)
+                try:
+                    modifiedMessage, serverAddress = playerSocket.recvfrom(2048)
+                    returnmessege = modifiedMessage.decode()
+                    print(returnmessege)
+                    playerSocket.close()
+                except:
+                    print("Wasn't picked up for game.")
+                    messege = "waiting," + playerName
+                    clientSocket = socket(AF_INET, SOCK_DGRAM)
+                    clientSocket.sendto(messege.encode(),(managerName, managerPort))
+                    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+                    clientSocket.close()
+            else:
+                print("Soemthing went wrong")
+        else:
+            print("Error: Must be registered to play in a game.")
+        userIn = input('Enter Command:')
+
+    elif userIn == '0':
         break
     else:
         print("Error: Please use a vaild input.")
         userIn = input('Enter Command:')
+
